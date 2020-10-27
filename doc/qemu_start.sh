4aaaae765a72493e6684a1a390209f092bffaa32
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2020-10-27 14:43:04 +0200

    [scripts] Add script to run VM in qemu
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ doc/qemu_start.sh
@@ -0,0 +1,18 @@
+# !/bin/sh
+
+script=$(basename -- "$0")
+
+if [ "$#" -lt 1 ]; then
+	echo "Expect 1 parameter. Usage example: ./${script} <image_path>"
+	exit 1
+fi
+
+qemu-system-x86_64 \
+	-device e1000,netdev=net0 \
+	-netdev user,id=net0,hostfwd=tcp::2222-:22 \
+	-hda ${1} \
+	-cpu host \
+	-enable-kvm \
+	-m 2G \
+	-drive if=pflash,format=raw,readonly,file=/usr/share/OVMF/OVMF_CODE.fd \
+	-boot menu=on
