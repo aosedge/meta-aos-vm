al1img/meta-aos-minimal

[SM,CM,UM] Switch to development branches
Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

recipes-aos/aos-servicemanager/aos-servicemanager_git.bbappend
@@ -1,5 +1,7 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
 
+BRANCH = "umctrl_impl"
+
 SRC_URI_append = " \
     file://aos_servicemanager.cfg \
     file://aos-servicemanager.service \

aosedge/meta-aos-vm

recipes-aos/aos-servicemanager/aos-servicemanager_git.bbappend
@@ -1,5 +1,7 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
 
+BRANCH = "update_v3"
+
 SRC_URI_append = " \
     file://aos_servicemanager.cfg \
     file://aos-servicemanager.service \