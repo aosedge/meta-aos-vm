c1a8bb64acf6175120b3e1882457abe3598a3b60
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2022-04-22 13:45:45 +0300

    [iam] Remove local provisioning firewall and use one from meta-aos
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- recipes-aos/aos-iamanager/files/iptables-rules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ls -d /sys/class/net/*/device | cut -d/ -f5 |
-while read -r line; do
-    iptables -A INPUT -i $line -p tcp --dport 8089 -j DROP
-    iptables -A INPUT -i $line -p udp --dport 8089 -j DROP
-done
