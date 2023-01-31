6b2f0182631c55906f3243207d000080d9c368ec
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2023-01-31 14:18:08 +0200

    [all] Switch root disk to /dev/sda instead of /dev/hda
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- doc/run_aos_vm.sh
+++ doc/run_aos_vm.sh
@@ -33,7 +33,8 @@ start_node() {
     mkdir -p /tmp/aos-vm/
 
     qemu-system-x86_64 \
-        -name $node -hda $node_image \
+        -name $node -drive file=$node_image,if=none,id=root-image \
+        -device ahci,id=ahci -device ide-hd,drive=root-image,bus=ahci.0 \
         -cpu host -smp cpus=$cpu -m $mem -enable-kvm \
         -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE.fd \
         -nic bridge,br=$bridge_name,model=virtio-net-pci,mac=$mac \
