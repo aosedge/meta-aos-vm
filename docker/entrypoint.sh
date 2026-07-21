199dd30ea54f405fc5fa3d6d3ab0daf05cbbe92a
Vasyl Samoilov <vasyl.samoilov@gmail.com>
2026-07-21 15:13:44 +0300

    ci: fix github 403 errors in docker build and yocto layer fetches

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,17 @@
 
 # Configure git to use GITHUB_TOKEN for github.com HTTPS access
 if [ -n "$GITHUB_TOKEN" ]; then
-    git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
+    # Store credentials in git credential store format
+    CRED_FILE="/tmp/.git-credentials"
+    echo "https://x-access-token:${GITHUB_TOKEN}@github.com" > "$CRED_FILE"
+    chmod 600 "$CRED_FILE"
+
+    GIT_CONF="/tmp/.gitconfig"
+    git config -f "$GIT_CONF" credential.helper "store --file=$CRED_FILE"
+    export GIT_CONFIG_GLOBAL="$GIT_CONF"
+
+    # Clear from env so child processes don't inherit it
+    unset GITHUB_TOKEN
 fi
 
 exec "$@"
